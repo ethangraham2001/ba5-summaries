bdd81f60e006d3d8d7f464f6cccbf6d9e2629bf6
Ethan <ethan.graham@epfl.ch>
2023-12-16 17:56:00 +0100

    updated formatting compile.py to fit within 80 cols

--- compile.py
+++ compile.py
@@ -10,7 +10,8 @@ if not(os.path.exists(PDF_DIR) and os.path.isdir(PDF_DIR)):
     os.mkdir(PDF_DIR)
 
 
-markdown_list = [file for file in os.listdir(MARKDOWN_DIR) if file.endswith('.md')]
+markdown_list = [file for file in os.listdir(MARKDOWN_DIR) \
+        if file.endswith('.md')]
 
 print(Style.BRIGHT + "Generating PDF files..." + Style.RESET_ALL)
 for md_file in markdown_list:
@@ -18,4 +19,5 @@ for md_file in markdown_list:
     pdf_file = md_file[:n-2]+'pdf'
     print(f'{Fore.CYAN}    --> Generating {pdf_file}{Fore.RED}')
     command = f'pandoc {MARKDOWN_DIR}/{md_file} -o {PDF_DIR}/{pdf_file}'
-    output = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
+    output = subprocess.run(command, shell=True, stdout=subprocess.PIPE, \
+            text=True)
